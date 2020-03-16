127c674a46f70e81878bd12cebba71fc3dca81f8
nashmutazu <44414714+nashmutazu@users.noreply.github.com>
2020-03-16 15:35:29 +0000

    Add files via upload

---
+++ script.py
@@ -0,0 +1,57 @@
+# Building a CNN 
+from tensorflow.python import keras
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Convolution2D
+from tensorflow.python.keras.layers import MaxPooling2D
+from tensorflow.python.keras.layers import Flatten
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution 
+classifier.add(Convolution2D(32, 3, 3, input_shape=(64, 64, 3), activation='relu'))
+
+# Step 2 - Pooling 
+classifier.add(MaxPooling2D(pool_size=(2,2)))
+
+# # Adding a second convolutional layer
+# classifier.add(Convolution2D(32, 3, 3, activation='relu'))
+# classifier.add(MaxPooling2D(pool_size=(2,2)))
+
+# Step 3 - Flattening 
+classifier.add(Flatten())
+
+# Step 4 - Full connection, look to use an output dim that is a power of two
+classifier.add(Dense(units=128, activation='relu'))
+classifier.add(Dense(units=1, activation='sigmoid'))
+
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_set = train_datagen.flow_from_directory(
+        'dataset/training_set',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='binary')
+
+test_set = test_datagen.flow_from_directory(
+        'dataset/test_set',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='binary')
+
+classifier.fit_generator(
+        train_set,
+        steps_per_epoch=8000,
+        epochs=15,
+        validation_data=test_set,
+        validation_steps=2000)
